84ce9b37c0e21fbba84a78e585ec7594f9679175
a1eksandrie <s.zus369@yandex.ru>
2026-09-20 11:57:39 +0300

    Add files via upload

---
+++ lab1/task1.cpp
@@ -0,0 +1,26 @@
+﻿using namespace std;
+#include <iostream>
+#include <omp.h>          
+
+int main(int argc, char* argv[]) {
+
+    int num_threads;
+    if (argc > 1) {
+        num_threads = atoi(argv[1]);
+    }
+        
+    omp_set_num_threads(num_threads);   // Устанавливаем количество потоков
+        
+#pragma omp parallel                    // Параллельная область (здесь создаются потоки)
+    {
+        int id = omp_get_thread_num();  // Каждый поток получает свой уникальный номер (0, 1, 2, ...)
+        int nthreads = omp_get_num_threads();       // Общее количество потоков в этой параллельной области
+        cout << "Hello World from thread " << id    // Печатаем (порядок вывода не гарантирован)
+            << " of " << nthreads << endl;
+    }
+    // После закрывающей скобки все потоки (кроме главного) завершаются
+
+    return 0;
+}
+
+
---
+++ lab1/task2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <cstdlib>
+#include <vector>
+#include <iomanip> 
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <num_threads> <array_size>\n";
+        return 1;
+    }
+
+    int num_threads = atoi(argv[1]);
+    int N = atoi(argv[2]);          // размер массива (для задачи = 16000)
+
+    if (num_threads <= 0 || N < 3) {
+        cerr << "Bad arguments\n";
+        return 1;
+    }
+
+    omp_set_num_threads(num_threads);
+
+    // Создаём и инициализируем массивы
+    vector<double> a(N);
+    vector<double> b(N, 0.0);       // сразу заполняем нулями
+
+    // Инициализация a[i] = i  (можно параллельно, но для простоты последовательно)
+    for (int i = 0; i < N; ++i) {
+        a[i] = static_cast<double>(i);
+    }
+
+    // Параллельный цикл с разными schedule (несколько запусков с заменой)
+
+#pragma omp parallel for schedule(guided)   // Вариант 1: static (по умолчанию)
+    for (int i = 1; i < N - 1; ++i) {
+        b[i] = (a[i - 1] + a[i] + a[i + 1]) / 3.0;
+    }
+
+    // Печатаем несколько первых и последних элементов
+    cout << "=== schedule(guided) ===\n";
+    cout << fixed << setprecision(2);
+    for (int i = 0; i < 5; ++i)
+        cout << "b[" << i << "] = " << b[i] << "\n";
+    cout << "...\n";
+    for (int i = N - 5; i < N; ++i)
+        cout << "b[" << i << "] = " << b[i] << "\n";
+
+    return 0;
+}
